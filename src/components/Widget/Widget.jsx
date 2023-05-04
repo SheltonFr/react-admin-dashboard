581a53ccf1cdf6c1a59ee5357cab3fce3f03170e
Shelton Francisco <sheltonfjs@gmail.com>
2023-05-04 09:35:55 +0200

    feat: im done ehhh

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,13 +1,15 @@
 import { AccountBalanceWalletOutlined, KeyboardArrowUp, MonetizationOnOutlined, PersonOutlineOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 import './widget.scss'
+import { useEffect, useState } from 'react';
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore'
+import { db } from '../../firebase'
 
 const Widget = ({ type }) => {
 
-    let data;
+    const [amount, setAmount] = useState(null)
+    const [percentage, setPercentage] = useState(null)
 
-    //temporary
-    const amount = 100;
-    const percentage = 20
+    let data;
 
     switch (type) {
         case 'user':
@@ -70,6 +72,37 @@ const Widget = ({ type }) => {
         default:
             break;
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const today = new Date();
+            const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+            const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+
+            const lastMonthQuery = query(
+                collection(db, 'users'),
+                where("timeStamp", "<=", today),
+                where("timeStamp", ">", lastMonth)
+            );
+
+            const prevMonthQuery = query(
+                collection(db, 'users'),
+                where("timeStamp", "<=", lastMonth),
+                where("timeStamp", ">", prevMonth)
+            );
+
+            const lastMonthData = await getDocs(lastMonthQuery)
+            const prevMonthData = await getDocs(prevMonthQuery)
+
+            setAmount(lastMonthData.docs.length)
+            setPercentage(
+                ((lastMonthData.docs.length - prevMonthData.docs.length) / prevMonthData.docs.length) * 100
+            )
+
+        }
+
+        fetchData();
+    }, [])
 
 
     return (
